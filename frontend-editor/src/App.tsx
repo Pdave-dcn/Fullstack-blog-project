77187cf0d2c10a7f129c2ecfa1b5262c1a8c8bd7
Pdave <ferdinandpdave@gmail.com>
2025-05-15 15:20:04 -0300

    Install and configure shadCn UI in frontend-editor directory

--- frontend-editor/src/App.tsx
+++ frontend-editor/src/App.tsx
@@ -1,5 +1,12 @@
+import { Button } from "./components/ui/button";
+
 const App = () => {
-  return <div className="text-2xl">Frontend Editor</div>;
+  return (
+    <>
+      <div className="text-2xl">Frontend Editor</div>
+      <Button>Click me</Button>
+    </>
+  );
 };
 
 export default App;
