d023e146394f97d6bd6ed7843ff63deb97c30f2a
Pdave <ferdinandpdave@gmail.com>
2025-12-28 17:41:03 -0500

    refactor: update requesterRole type in DeleteCommentCommand to use UserRole enum

--- api/src/application/comments/delete/DeleteCommentCommand.ts
+++ api/src/application/comments/delete/DeleteCommentCommand.ts
@@ -1,5 +1,7 @@
+import { UserRole } from "@/domains/users/UserRole";
+
 export interface DeleteCommentCommand {
   commentId: string;
   requesterId: string;
-  requesterRole: "AUTHOR" | "READER";
+  requesterRole: UserRole;
 }
